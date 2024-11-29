39c91fb013385c12f266c61238f3394703e46476
akr13 <akregmi@gmail.com>
2024-11-29 13:54:18 -0500

    Changes

---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return(
+        <div className="flex md:flex-row flex-col">
+    
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/components/consts.js
@@ -0,0 +1,22 @@
+export const NavLinks = [
+    {
+        id: 'home',
+        title: 'Home',
+    },
+    {
+        id: 'about',
+        title: 'About',
+    },
+    {
+        id: 'experience',
+        title: 'Experience',
+    },
+    {
+        id: 'projects',
+        title: 'Projects',
+    },
+    {
+        id: 'contact',
+        title: 'Contact',
+    }
+]
